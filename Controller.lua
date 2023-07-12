c535ef367de747cf921e6af62785799d7abeb984
Plixit7007 <77698977+Plixit7007@users.noreply.github.com>
2023-07-12 19:11:21 -0400

    Create Controller.lua

---
+++ Controller.lua
@@ -0,0 +1,34 @@
+local active = GetPort(1)
+local deactive = GetPort(2)
+local spinny = GetPartFromPort(3, "Motor")
+local speed = 0
+local incval = 1
+
+active:Connect("Triggered", function()
+    if speed ~= -1 and speed ~= 100 then
+        speed = speed + incval
+    end
+    if speed == -1 then
+        speed = 0
+    end
+    if speed == 101 then
+        speed = 100
+    end
+end)
+
+deactive:Connect("Triggered", function()
+    negativeinc = incval * -1
+    if speed ~= -1 and speed ~= 100 then
+        speed = speed + negativeinc
+    end
+    if speed == -1 then
+        speed = 0
+    end
+    if speed == 101 then
+        speed = 100
+    end
+end)
+
+while task.wait() do
+    spinny:Configure({ Ratio = speed })
+end
